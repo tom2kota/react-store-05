be01be2b6942687cd83d194c7fbbfc632d1ec394
Web Dev <tom2kota@gmail.com>
2020-09-24 15:04:40 +0300

    Fix persistConfig

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -9,7 +9,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['cart', 'directory', 'shop']
+    whitelist: ['cart']
 }
 
 const rootReducer = combineReducers({
